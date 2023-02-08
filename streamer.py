e97775ee3a7985484de866c956c5e8cbac39be91
Hannah Zimmerman <hannahzimmerman2023@u.northwestern.edu>
2023-02-08 03:07:23 -0600

    Part 1 Chunking

--- streamer.py
+++ streamer.py
@@ -17,9 +17,14 @@ class Streamer:
     def send(self, data_bytes: bytes) -> None:
         """Note that data_bytes can be larger than one packet."""
         # Your code goes here!  The code below should be changed!
-
-        # for now I'm just sending the raw application-level data in one UDP payload
-        self.socket.sendto(data_bytes, (self.dst_ip, self.dst_port))
+        message = []
+        while len(data_bytes) >= 1472:
+            message.append(data_bytes[:1472])
+            data_bytes = data_bytes[1472:]
+        if len(data_bytes) > 0:
+            message.append(data_bytes)
+        for packet in message:
+            self.socket.sendto(packet, (self.dst_ip, self.dst_port))
 
     def recv(self) -> bytes:
         """Blocks (waits) if no data is ready to be read from the connection."""
