fad4f751805a2d317cdfc4f246b6448954544212
Hannah Zimmerman <hannahzimmerman2023@u.northwestern.edu>
2023-02-09 16:50:53 -0600

    Part 3B

--- streamer.py
+++ streamer.py
@@ -4,6 +4,7 @@ from lossy_socket import LossyUDP
 from socket import INADDR_ANY
 import struct
 from concurrent.futures import ThreadPoolExecutor
+import time
 
 
 class Streamer:
@@ -12,12 +13,22 @@ class Streamer:
         while not self.closed:
             try:
                 data, addr = self.socket.recvfrom()
-                if data != b"":
-                    self.recv_seq = struct.unpack('i', data[:4])[0]
-                    self.receive_buffer.append([self.recv_seq, data[4:]])
-                    self.receive_buffer.sort(key=lambda x: x[0])
+                if data:
+                    header = struct.unpack('i?', data[:self.header_size])
+                    is_ack = header[1]
+                    if is_ack:
+                        ack_seq = header[0]
+                        self.acks.append(ack_seq)
+                    else:
+                        self.recv_seq = header[0]
+                        self.receive_buffer.append([self.recv_seq, data[self.header_size:]])
+                        self.receive_buffer.sort(key=lambda x: x[0])
+                        header = struct.pack('i?', self.recv_seq+1, True)  # int = 4 bytes, bool = 1 byte
+                        packet = header + "ACK".encode()
+                        self.socket.sendto(packet, (self.dst_ip, self.dst_port))
             except Exception as e:
                 print("listener died!")
+                print(data[:5])
                 print(e)
 
     def __init__(self, dst_ip, dst_port,
@@ -32,23 +43,29 @@ class Streamer:
         self.sent_seq = 0
         self.recv_seq = -1
         self.next_seq = 0
+        self.acks = []
         self.closed = False
+        self.header_size = 5
         executor = ThreadPoolExecutor(max_workers=1)
         executor.submit(self.listener)
 
     def send(self, data_bytes: bytes) -> None:
         """Note that data_bytes can be larger than one packet."""
         message = []
-        while len(data_bytes) >= 1468:
-            message.append(data_bytes[:1468])
-            data_bytes = data_bytes[1468:]
+        max_packet_size = 1472-self.header_size
+        while len(data_bytes) >= max_packet_size:
+            message.append(data_bytes[:max_packet_size])
+            data_bytes = data_bytes[max_packet_size:]
         if len(data_bytes) > 0:
             message.append(data_bytes)
         for i in range(len(message)):
-            header = struct.pack('i', self.sent_seq + i)  # int = 4 bytes
+            header = struct.pack('i?', self.sent_seq + i, False)  # int = 4 bytes, bool = 1 byte
             packet = header + message[i]
             self.socket.sendto(packet, (self.dst_ip, self.dst_port))
+        ack_seq = self.sent_seq
         self.sent_seq += len(message)
+        while ack_seq+1 not in self.acks: time.sleep(0.01)
+        self.acks.remove(ack_seq+1)
 
     def recv(self) -> bytes:
         """Blocks (waits) if no data is ready to be read from the connection."""
