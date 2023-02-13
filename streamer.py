3e570cc39abee5be02f7bdd75c9231ab94392974
Hannah Zimmerman <hannahzimmerman2023@u.northwestern.edu>
2023-02-13 00:19:51 -0600

    Part 3C ACK timeout

--- streamer.py
+++ streamer.py
@@ -20,15 +20,17 @@ class Streamer:
                         ack_seq = header[0]
                         self.acks.append(ack_seq)
                     else:
-                        self.recv_seq = header[0]
-                        self.receive_buffer.append([self.recv_seq, data[self.header_size:]])
+                        recv_seq = header[0]
+                        recv_log = [recv_seq, data[self.header_size:]]
+                        if recv_log not in self.receive_buffer and recv_seq >= self.next_seq:
+                            self.receive_buffer.append(recv_log)
                         self.receive_buffer.sort(key=lambda x: x[0])
-                        header = struct.pack('i?', self.recv_seq+1, True)  # int = 4 bytes, bool = 1 byte
+                        header = struct.pack('i?', recv_seq+1, True)  # int = 4 bytes, bool = 1 byte
                         packet = header + "ACK".encode()
                         self.socket.sendto(packet, (self.dst_ip, self.dst_port))
+                        # print(f"sent ack for {recv_seq}")
             except Exception as e:
                 print("listener died!")
-                print(data[:5])
                 print(e)
 
     def __init__(self, dst_ip, dst_port,
@@ -39,11 +41,10 @@ class Streamer:
         self.socket.bind((src_ip, src_port))
         self.dst_ip = dst_ip
         self.dst_port = dst_port
-        self.receive_buffer = []
-        self.sent_seq = 0
-        self.recv_seq = -1
-        self.next_seq = 0
-        self.acks = []
+        self.receive_buffer = []  # received packets after first gap [seq, data]
+        self.sent_seq = 0  # all packets up to this number have been sent
+        self.next_seq = 0  # lowest seq not yet received
+        self.acks = []  # sent packets that have received ACKs before send finishes
         self.closed = False
         self.header_size = 5
         executor = ThreadPoolExecutor(max_workers=1)
@@ -58,26 +59,37 @@ class Streamer:
             data_bytes = data_bytes[max_packet_size:]
         if len(data_bytes) > 0:
             message.append(data_bytes)
+        message_acks = [[0, 0] for i in range(len(message))]
+        packets = [b"" for i in range(len(message))]
         for i in range(len(message)):
             header = struct.pack('i?', self.sent_seq + i, False)  # int = 4 bytes, bool = 1 byte
             packet = header + message[i]
+            packets[i] = packet
             self.socket.sendto(packet, (self.dst_ip, self.dst_port))
-        ack_seq = self.sent_seq
+            message_acks[i] = [self.sent_seq+i+1, time.time()]
+        while not set([i[0] for i in message_acks]).issubset(self.acks):
+            no_ack = [i for i in message_acks if i[0] not in self.acks]
+            for i in no_ack:
+                if time.time()-i[1] > 0.25:
+                    self.socket.sendto(packets[i[0]-self.sent_seq-1], (self.dst_ip, self.dst_port))
+                    i[1] = time.time()
+            time.sleep(0.01)
+        self.acks = [seq for seq in self.acks if seq not in [i[0] for i in message_acks]]
         self.sent_seq += len(message)
-        while ack_seq+1 not in self.acks: time.sleep(0.01)
-        self.acks.remove(ack_seq+1)
 
     def recv(self) -> bytes:
         """Blocks (waits) if no data is ready to be read from the connection."""
+        """While buffer empty or gap at beginning of buffer"""
         while len(self.receive_buffer) == 0 or self.receive_buffer[0][0] != self.next_seq:
             continue
         i = 0
         message = b""
+        """While i's seq # is 1 more than i-1's seq #"""
         while i != len(self.receive_buffer) and (
                 i == 0 or self.receive_buffer[i][0] == self.receive_buffer[i - 1][0] + 1):
             message += self.receive_buffer[i][1]
             i += 1
-        self.receive_buffer = self.receive_buffer[i:]
+        self.receive_buffer = self.receive_buffer[i+1:]
         self.next_seq += i
         return message
 
